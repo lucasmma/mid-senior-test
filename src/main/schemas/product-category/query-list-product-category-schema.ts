c963b9a648caad16c2bdbfb9714b7e70f22eaa27
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 12:51:54 -0300

    chore: remove unnecessary schemas

--- src/main/schemas/product-category/query-list-product-category-schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-
-export const queryListProductCategorySchema = z.object({
-  include_products: z.string()
-    .transform((val) => val === 'true')
-    .optional()
-}).strict();
