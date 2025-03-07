1623327b6b11bd46b7c368333a97901c6fb767b2
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 14:47:53 -0300

    feat: create updateLoanStatusSchema

---
+++ src/main/schemas/loan/update-loan-status-schema.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     UpdateLoanStatus:
+ *       type: object
+ *       properties:
+ *          status:
+ *            type: string
+ *            enum: [APPROVED, REJECTED]
+ * 
+ *       required:
+ *        - status
+ *  
+ *       additionalProperties: false
+ *       description: |
+ *          Schema for updating status of one loan.
+ *          - `status`: The amount of the loan.
+ *       example:
+ *         status: APPROVED
+ */
+export const updateLoanStatusSchema = z.object({
+  status: z.enum(['APPROVED', 'REJECTED']),
+});
