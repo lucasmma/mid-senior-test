dd7e30c197c7c23571e7d5cec8552af6af38748b
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 14:26:52 -0300

    feat: add to routes listById loan endpoint

--- src/main/routes/loan-routes.ts
+++ src/main/routes/loan-routes.ts
@@ -3,10 +3,13 @@ import { adaptRoute } from '../route-adapters/controller-route-adapter'
 import { makeLoanController } from '../factories/loan-factory'
 import { createLoanSchema } from '../schemas/loan/create-loan-schema'
 import { authorization } from '../middlewares/auth'
+import { idSchema } from '../schemas/id-schema'
 
 export default (router: Router): void => {
   const baseRoute = '/loans'
   const controller = makeLoanController()
 
   router.post(baseRoute, authorization('USER'), adaptRoute(controller, controller.createLoan, { body: createLoanSchema }))
+  router.get(baseRoute, authorization('USER'), adaptRoute(controller, controller.listLoans))
+  router.get(baseRoute + '/:id', authorization('USER'), adaptRoute(controller, controller.listLoan, { param: idSchema }))
 }
