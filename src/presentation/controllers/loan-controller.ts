9285fbbc0853fa0086e04a07e74450bab420b632
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 14:09:28 -0300

    feat: create loan controller and function to create entry

---
+++ src/presentation/controllers/loan-controller.ts
@@ -0,0 +1,32 @@
+import { HttpRequest, HttpResponse } from '../protocols'
+import { badRequest, ok } from '../helpers/http-helper'
+import prisma from '../../main/config/prisma'
+import { JwtAdapter } from '../../infra/auth/jwt-adapter'
+import { omit } from '../helpers/omit-field'
+import { createLoanSchema } from '../../main/schemas/loan/create-loan-schema'
+
+export class LoanController {
+  constructor() {
+  }
+  async create(
+    request: HttpRequest<( typeof createLoanSchema._output)>,
+  ): Promise<HttpResponse> {
+    const body = request.body!
+    var user = request.auth!.user!
+
+    const loan = prisma.loan.create({
+      data: {
+        amount: body.amount,
+        purpose: body.purpose,
+        duration: body.duration,
+        user_id: user.id,
+        status: 'PENDING',
+        total_paid: 0,
+        remaining_balance: 0,
+      }
+    })
+
+
+    return ok(loan)
+  }
+}
