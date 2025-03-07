b44e61054d0d621f9a4eb7c3c2cd8702ae69b3e7
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 16:24:52 -0300

    feat: add payment routes to routes

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Router } from 'express'
 import userRoutes from '../routes/user-routes'
 import loanRoutes from '../routes/loan-routes'
+import paymentRoutes from '../routes/payment-routes'
 
 export default (router: Router): void => {
   userRoutes(router)
   loanRoutes(router)
+  paymentRoutes(router)
 }
