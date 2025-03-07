bbd2c79f0db199eedb6dd7cb5bf1db7aa71a8ccc
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 15:23:47 -0300

    feat: add loan routes to router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,6 +1,8 @@
 import { Express, Router } from 'express'
 import userRoutes from '../routes/user-routes'
+import loanRoutes from '../routes/loan-routes'
 
 export default (router: Router): void => {
   userRoutes(router)
+  loanRoutes(router)
 }
