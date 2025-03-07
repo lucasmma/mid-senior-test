1ddb6181e5b4b91b03c226cb854da7bdc6ac94b1
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 11:58:24 -0300

    chore: remove unnecessary files

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,14 +1,6 @@
 import { Express, Router } from 'express'
 import userRoutes from '../routes/user-routes'
-import productRoutes from '../routes/product-routes'
-import productCategoryRoutes from '../routes/product-category-route'
-import orderRoutes from '../routes/order-routes'
-import stockRoutes from '../routes/stock-routes'
 
 export default (router: Router): void => {
   userRoutes(router)
-  productRoutes(router)
-  productCategoryRoutes(router)
-  orderRoutes(router)
-  stockRoutes(router)
 }
--- src/main/factories/stock-factory.ts
+++
@@ -1,7 +0,0 @@
-import { DbStockMethods } from '../../data/usecases/db-stock-methods'
-import { StockController } from '../../presentation/controllers/stock-controller'
-
-export function makeStockController(): StockController {
-  var stockMethods = new DbStockMethods()
-  return new StockController(stockMethods)
-}
