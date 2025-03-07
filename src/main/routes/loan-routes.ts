4538f229413a288e4cac4fabf055edf55a70dd0a
Lucas Mendonça <lukito020@gmail.com>
2025-03-07 15:29:21 -0300

    feat: add correct body schema on updateLoanStatus endpoint

--- src/main/routes/loan-routes.ts
+++ src/main/routes/loan-routes.ts
@@ -4,6 +4,7 @@ import { makeLoanController } from '../factories/loan-factory'
 import { createLoanSchema } from '../schemas/loan/create-loan-schema'
 import { authorization } from '../middlewares/auth'
 import { idSchema } from '../schemas/id-schema'
+import { updateLoanStatusSchema } from '../schemas/loan/update-loan-status-schema'
 
 export default (router: Router): void => {
   const baseRoute = '/loans'
@@ -12,5 +13,5 @@ export default (router: Router): void => {
   router.post(baseRoute, authorization('USER'), adaptRoute(controller, controller.createLoan, { body: createLoanSchema }))
   router.get(baseRoute, authorization('USER'), adaptRoute(controller, controller.listLoans))
   router.get(baseRoute + '/:id', authorization('USER'), adaptRoute(controller, controller.listLoan, { param: idSchema }))
-  router.patch(baseRoute + '/:id/status', authorization('ADMIN'), adaptRoute(controller, controller.updateStatus, { param: idSchema }))
+  router.patch(baseRoute + '/:id/status', authorization('ADMIN'), adaptRoute(controller, controller.updateStatus, { body: updateLoanStatusSchema , param: idSchema }))
 }
