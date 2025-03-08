4f75b8dd2001f006c17a0731487bdeeaa9849ab4
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 12:06:17 -0300

    refactor: remove unnecessary function

--- src/data/protocols/db-loan-repository.ts
+++ src/data/protocols/db-loan-repository.ts
@@ -3,5 +3,4 @@ import { Loan } from '@prisma/client'
 export interface DbLoanRepository {
   getLoanById(loanId: string): Promise<Loan | null>
   updateLoan(loanId: string, data: Partial<Omit<Loan, 'id'>>): Promise<Loan | null>
-  getLoansByUserId(userId: string): Promise<Loan[]>
 }
--- src/repository/loan-repository.ts
+++ src/repository/loan-repository.ts
@@ -3,16 +3,6 @@ import { DbLoanRepository } from '../data/protocols/db-loan-repository'
 import prisma from '../main/config/prisma'
 
 export class LoanRepository implements DbLoanRepository {
-  async getLoansByUserId(userId: string): Promise<Loan[]> {
-    // implementation
-    const loans = await prisma.loan.findMany({
-      where: {
-        user_id: userId
-      }
-    })
-    return loans
-  }
-
   async getLoanById(loanId: string): Promise<Loan | null> {
     // implementation
     const loan = await prisma.loan.findUnique({
