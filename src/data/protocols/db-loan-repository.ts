be9f9f4e08da00aedce16007aff9e1913174a33f
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 11:57:58 -0300

    feat: create dbLoanRepository

---
+++ src/data/protocols/db-loan-repository.ts
@@ -0,0 +1,7 @@
+import { Loan } from '@prisma/client'
+
+export interface DbLoanRepository {
+  getLoanById(loanId: string): Promise<Loan | null>
+  updateLoan(loanId: string, data: Partial<Omit<Loan, 'id'>>): Promise<Loan | null>
+  getLoansByUserId(userId: string): Promise<Loan[]>
+}
