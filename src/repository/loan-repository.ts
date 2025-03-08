10d2809f6fa42d3ba3ffb7355520592aa5c7395b
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 12:14:14 -0300

    feat: add loanCache to loanRepository

--- src/repository/loan-repository.ts
+++ src/repository/loan-repository.ts
@@ -1,8 +1,12 @@
 import { Loan } from '@prisma/client'
 import { DbLoanRepository } from '../data/protocols/db-loan-repository'
 import prisma from '../main/config/prisma'
+import { CacheAdapter } from '../infra/cache/cache-adapter'
 
 export class LoanRepository implements DbLoanRepository {
+  constructor(private readonly loanCache: CacheAdapter) {
+    this.loanCache = loanCache
+  }
   async getLoanById(loanId: string): Promise<Loan | null> {
     // implementation
     const loan = await prisma.loan.findUnique({
