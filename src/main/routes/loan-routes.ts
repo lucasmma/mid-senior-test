7bd361ac21f36efdfcdcec2f7a835352aeceaa5f
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 13:47:13 -0300

    docs: create swagger for loan routes

--- src/main/routes/loan-routes.ts
+++ src/main/routes/loan-routes.ts
@@ -7,6 +7,102 @@ import { idSchema } from '../schemas/id-schema'
 import { updateLoanStatusSchema } from '../schemas/loan/update-loan-status-schema'
 import { paginationSchema } from '../schemas/pagination-schema'
 
+/**
+ * @openapi
+ * tags:
+ *   name: Loans
+ *   description: API for loans in the system
+ * 
+ * /loans:
+ *   post:
+ *     tags:
+ *       - Loans
+ *     summary: Create a loan
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateLoan'
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Server Error
+ * 
+ *   get:
+ *     tags:
+ *       - Loans
+ *     summary: List loans
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - $ref: '#/components/parameters/SkipParameter'
+ *       - $ref: '#/components/parameters/LimitParameter'
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Server Error
+ * 
+ * /loans/{id}:
+ *   get:
+ *     tags:
+ *       - Loans
+ *     summary: Get loan
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           $ref: '#/components/parameters/IdParameter'
+ *         description: Loan ID
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server Error
+ * 
+ * /loans/{id}/status:
+ *   patch:
+ *     tags:
+ *       - Loans
+ *     summary: Update loan status
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           $ref: '#/components/parameters/IdParameter'
+ *         description: Loan ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateLoanStatus'
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *       500:
+ *         description: Server Error
+ */
+
+
 export default (router: Router): void => {
   const baseRoute = '/loans'
   const controller = makeLoanController()
