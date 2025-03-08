8c351c0432b091c07fe2438f6d84873127e4a6b0
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 12:06:29 -0300

    feat: initialize loanRepository and pass to loanController

--- src/main/factories/loan-factory.ts
+++ src/main/factories/loan-factory.ts
@@ -1,5 +1,7 @@
 import { LoanController } from '../../presentation/controllers/loan-controller'
+import { LoanRepository } from '../../repository/loan-repository'
 
 export function makeLoanController(): LoanController {
-  return new LoanController()
+  const loanRepository = new LoanRepository
+  return new LoanController(loanRepository)
 }
