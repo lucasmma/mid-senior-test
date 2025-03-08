6149c16d6b49a24ad46bbadab0de9671af35385a
Lucas Mendonça <lukito020@gmail.com>
2025-03-08 12:19:19 -0300

    feat: add function to create loan on LoanRepository

--- src/data/protocols/db-loan-repository.ts
+++ src/data/protocols/db-loan-repository.ts
@@ -3,4 +3,5 @@ import { Loan } from '@prisma/client'
 export interface DbLoanRepository {
   getLoanById(loanId: string): Promise<Loan | null>
   updateLoan(loanId: string, data: Partial<Omit<Loan, 'id'>>): Promise<Loan | null>
+  createLoan(data: {amount: number, purpose: string, duration: number, user_id: string}): Promise<Loan>
 }
--- src/repository/loan-repository.ts
+++ src/repository/loan-repository.ts
@@ -7,6 +7,23 @@ export class LoanRepository implements DbLoanRepository {
   constructor(private readonly loanCache: CacheAdapter) {
     this.loanCache = loanCache
   }
+
+  createLoan(data: {amount: number, purpose: string, duration: number, user_id: string}): Promise<Loan> {
+    // implementation
+    const loan = prisma.loan.create({
+      data: {
+        amount: data.amount,
+        purpose: data.purpose,
+        duration: data.duration,
+        user_id: data.user_id,
+        status: 'PENDING',
+        total_paid: 0,
+        remaining_balance: 0,
+      },
+    })
+    return loan
+  }
+
   async getLoanById(loanId: string): Promise<Loan | null> {
     // implementation
     const loan = await prisma.loan.findUnique({
